9b4ccede91cac07cc48d7695c24dc507a15c89f3
Guillaume <guillaume.assfeld@epitech.eu>
2020-12-15 10:46:45 +0100

    update signature to NODE

--- Functions/signature.js
+++ Functions/signature.js
@@ -1,20 +1,28 @@
-var crypto = require("crypto");
-var eccrypto = require("eccrypto");
+const crypto = require('crypto')
+const fs = require('fs')
 
-// A new random 32-byte private key.
-var privateKey = Buffer.from('2fd7cab0970c692b4151d77a6aeebcae2a3284556cbfc6f182c571eccfc2424f', 'hex')
-// Corresponding uncompressed (65-byte) public key.
-var publicKey = eccrypto.getPublic(privateKey);
+const getSignatureByInput = (input) => {
+  let privatePem = fs.readFileSync('PRIVATE_KEY_FILE_PATH_GOES_HERE')
+  let key = privatePem.toString('ascii')
+  let sign = crypto.createSign('RSA-SHA256')
+  sign.update(input)
+  let signature = sign.sign(key, 'hex')
 
-var buf = Buffer.from('05de69f5d37f41340eed3230f03d2394dde5e497738a76f027b7d962a0cbdf39', 'hex')
-// Always hash you message to sign!
-var msg = crypto.createHash("sha256").update(buf).digest();
+  return signature
+}
 
-eccrypto.sign(privateKey, buf).then(function(sig) {
-  console.log("Signature in DER format (hex):", sig.toString('hex'))
-  eccrypto.verify(publicKey, buf, sig).then(function() {
-    console.log("Signature is OK");
-  }).catch(function() {
-    console.log("Signature is BAD");
-  });
-});
+const getSignatureVerifyResult = (input) => {
+        let signatureSignedByPrivateKey = getSignatureByInput(input)
+
+        let pem = fs.readFileSync('PUBLIC_KEY_FILE_PATH_GOES_HERE')
+        let publicKey = pem.toString('ascii')
+        const verifier = crypto.createVerify('RSA-SHA256')
+
+        verifier.update(input, 'ascii')
+
+        const publicKeyBuf = new Buffer(publicKey, 'ascii')
+        const signatureBuf = new Buffer(signatureSignedByPrivateKey, 'hex')
+        const result = verifier.verify(publicKeyBuf, signatureBuf)
+
+        return result;
+}
